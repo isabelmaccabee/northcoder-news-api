194dd3586f0b72c050aee6f9edb5654e7947e1ac
isabelmaccabee <isabel.maccabee@gmail.com>
2018-11-20 14:34:27 +0000

    Fixing date

--- db/migrations/20181120111727_create_article_table.js
+++ db/migrations/20181120111727_create_article_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("articles", articlesTable => {
     articlesTable.increments("article_id").primary();
     articlesTable.string("title").notNullable();
-    articlesTable.string("body").notNullable();
+    articlesTable.string("body", 9999).notNullable();
     articlesTable
       .integer("votes")
       .notNullable()
--- db/utils/index.js
+++ db/utils/index.js
@@ -5,37 +5,16 @@ const formatArticleData = (rawArticleData, referenceObj) => {
       title,
       topic,
       body,
-      created_at: formatDate(created_at),
+      created_at: new Date(created_at),
       user_id: referenceObj[created_by]
     };
     return newObj;
   });
 };
 
-const formatDate = dateInSecs => {
-  const articleDate = new Date(dateInSecs);
-  const formattedDate = `${articleDate.getDate()}/${articleDate.getMonth()}/${articleDate.getFullYear()}`;
-  //   const month =
-  //     articleDate.getMonth().length === 1
-  //       ? `0${articleDate.getMonth()}`
-  //       : `${articleDate.getMonth()}`;
-  //   const day =
-  //     articleDate.getDate().length === 1
-  //       ? `0${articleDate.getDate()}`
-  //       : `${articleDate.getDate()}`;
-  //   const year =
-  //     articleDate.getYear().length === 1
-  //       ? `0${articleDate.getYear()}`
-  //       : `${articleDate.getYear()}`;
-  //   const formattedDate = `${year}/${month}/${day}`;
-  console.log(formattedDate);
-  return formattedDate;
-};
-
 const createReferenceObj = (rows, columnValue, idValue) => {
   return rows.reduce((refObj, row) => {
     refObj[row[columnValue]] = row[idValue];
-    // console.log(refObj);
     return refObj;
   }, {});
 };
