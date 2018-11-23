6f1cb279fa18f1a698d1f9d53b05c3e95b22afee
isabelmaccabee <isabel.maccabee@gmail.com>
2018-11-23 15:12:54 +0000

    console log

--- db/connection.js
+++ db/connection.js
@@ -16,4 +16,6 @@ const config = ENV === 'production'
   ? { client: 'pg', connection: process.env.DB_URL }
   : require('../knexfile')[ENV];
 
+console.log(process.env.DB_URL);
+
 module.exports = require('knex')(config);
