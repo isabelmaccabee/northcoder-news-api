99367646fdb35f1104f1fb0210a7a8df86062d8e
isabelmaccabee <isabel.maccabee@gmail.com>
2018-11-23 15:00:20 +0000

    Fixed small issue

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,19 @@
-const knex = require('knex');
-const config = require('../knexfile');
+// const knex = require('knex');
+
+// const ENV = process.env.NODE_ENV || 'development';
+// const config = ENV === 'production'
+//   ? { client: 'pg', connection: process.env.DB_URL }
+//   : require('../knexfile')[ENV];
+
+// // module.exports = require('knex')(config);
+
+// const connection = knex(config);
+
+// module.exports = connection;
 
 const ENV = process.env.NODE_ENV || 'development';
-const config =
-  ENV === 'production'
-    ? { client: 'pg', connection: process.env.DB_URL }
-    : require('../knexfile')[ENV];
+const config = ENV === 'production'
+  ? { client: 'pg', connection: process.env.DB_URL }
+  : require('../knexfile')[ENV];
 
-// module.exports = require('knex')(config);
-
-const connection = knex(config);
-
-module.exports = connection;
+module.exports = require('knex')(config);
